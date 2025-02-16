780844ba0748217972ea843b2a64163d85d0cab7
Fischer Davis <fischer.davis@icloud.com>
2025-02-16 09:23:32 -0600

    Added route to handle file deletes

--- src/server/api/routers/file.ts
+++ src/server/api/routers/file.ts
@@ -2,7 +2,9 @@ import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 import { z } from "zod";
 import { db } from "@/server/db";
 import { files } from "@/server/db/schema";
-import { desc } from "drizzle-orm";
+import { desc, inArray } from "drizzle-orm";
+import fs from "fs";
+import path from "path";
 
 export const fileRouter = createTRPCRouter({
   saveFileMetadata: publicProcedure
@@ -22,7 +24,43 @@ export const fileRouter = createTRPCRouter({
 
       return { success: true, file: newFile };
     }),
+  deleteFiles: publicProcedure
+    .input(z.array(z.string()))
+    .mutation(async ({ input }) => {
+      const fileRecords = await db
+        .select()
+        .from(files)
+        .where(inArray(files.id, input))
+        .all();
 
+      if (fileRecords.length === 0) {
+        return { success: false, error: "Files not found" };
+      }
+
+      const errors: { id: string; error: string }[] = [];
+
+      for (const fileRecord of fileRecords) {
+        const filePath = path.join(process.cwd(), fileRecord.filePath);
+
+        try {
+          fs.unlinkSync(filePath);
+        } catch (error) {
+          console.error("Error deleting file from file system:", error);
+          errors.push({
+            id: fileRecord.id,
+            error: "Error deleting file from file system",
+          });
+        }
+      }
+
+      await db.delete(files).where(inArray(files.id, input));
+
+      if (errors.length > 0) {
+        return { success: false, errors };
+      }
+
+      return { success: true };
+    }),
   getUploadedFiles: publicProcedure.query(async () => {
     return db.select().from(files).orderBy(desc(files.uploadedAt));
   }),
