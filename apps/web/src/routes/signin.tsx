c443316083e9dc3ea671a48a52deae2c85c96342
Fischer <fischer@Taylors-iMac.local>
2025-03-19 22:55:45 -0500

    Added dark mode to signin/signup

--- apps/web/src/routes/signin.tsx
+++ apps/web/src/routes/signin.tsx
@@ -1,4 +1,6 @@
 import Login from "@/components/login.tsx";
+import { ThemeSwitch } from "@/components/theme-switch.tsx";
+import { VisuallyHidden } from "@react-aria/visually-hidden";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/signin")({
@@ -8,6 +10,9 @@ export const Route = createFileRoute("/signin")({
 function SignIn() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <VisuallyHidden>
+        <ThemeSwitch />
+      </VisuallyHidden>
       <div className="w-full max-w-sm">
         <Login />
       </div>
--- apps/web/src/routes/signup.tsx
+++ apps/web/src/routes/signup.tsx
@@ -1,4 +1,6 @@
 import Register from "@/components/register.tsx";
+import { ThemeSwitch } from "@/components/theme-switch.tsx";
+import { VisuallyHidden } from "@react-aria/visually-hidden";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/signup")({
@@ -8,6 +10,9 @@ export const Route = createFileRoute("/signup")({
 function SignUp() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <VisuallyHidden>
+        <ThemeSwitch />
+      </VisuallyHidden>
       <div className="w-full max-w-sm">
         <Register />
       </div>
